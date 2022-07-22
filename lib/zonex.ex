7b694320e73f221270745e1d518ceeff5193752a
Derrick Reimer <derrickreimer@gmail.com>
2022-07-22 15:40:14 -0500

    Initial commit

---
+++ lib/zonex.ex
@@ -0,0 +1,18 @@
+defmodule Zonex do
+  @moduledoc """
+  Documentation for `Zonex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zonex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zonex_test.exs
@@ -0,0 +1,8 @@
+defmodule ZonexTest do
+  use ExUnit.Case
+  doctest Zonex
+
+  test "greets the world" do
+    assert Zonex.hello() == :world
+  end
+end
